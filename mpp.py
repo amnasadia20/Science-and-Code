95342aa0d5a1f766d59bd55c108577069cd7431d
amnasadia20 <murtazaamna117@gmail.com>
2026-06-27 13:23:40 +0500

    Mpp g

--- mpp.py
+++ mpp.py
@@ -15,3 +15,21 @@ int main() {
 
     cout << "Enter growth rate (e.g. 0.1 for 10%): ";
     cin >> growthRate;\
+cout << "Enter carrying capacity: ";
+    cin >> carryingCapacity;
+
+    cout << "Enter number of years to simulate: ";
+    cin >> years;
+
+    cout << "\nYear\tPopulation\n";
+
+    // Simulation loop
+    for (int year = 1; year <= years; year++) {
+        double change = growthRate * population * (1 - population / carryingCapacity);
+        population = population + change;
+
+        cout << year << "\t" << population << endl;
+    }
+
+    return 0;
+}
